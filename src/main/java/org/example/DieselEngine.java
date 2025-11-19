bdbb4c2c90980166802be453ab4f67c5c67d2b71
parasbramhankar <parasbramhankar.in@gmail.com>
2025-11-19 09:13:02 +0530

    commit

---
+++ src/main/java/org/example/DieselEngine.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class DieselEngine implements Engine {
+
+    public int start(){
+        System.out.println("Trying to start the Diesel engine");
+        return 1;
+    }
+
+}
