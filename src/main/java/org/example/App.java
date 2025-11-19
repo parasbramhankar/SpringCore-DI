41c7df2ce34095143cfeeb45ecf1d0e954ece08c
parasbramhankar <parasbramhankar.in@gmail.com>
2025-11-19 16:49:13 +0530

    commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,15 @@
 package org.example;
 
-public class App 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
 {
     public static void main( String[] args )
     {
-
+        //load Beans.xml
+        ApplicationContext context=new ClassPathXmlApplicationContext("Beans.xml");
+        Car car = context.getBean(Car.class);
+        car.drive();
     }
 }
