590a73794d8546794e0a10f5b1affcd170c1acaa
parasbramhankar <parasbramhankar.in@gmail.com>
2025-11-19 09:13:04 +0530

    commit

---
+++ src/main/java/org/example/PetrolEngine.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class PetrolEngine implements Engine{
+
+    public int start(){
+        System.out.println("Trying to start the petrol engine...");
+        return 1;
+    }
+}
