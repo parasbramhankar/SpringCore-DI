f8572f806ba468f36163a55538e51312fb198d0b
parasbramhankar <parasbramhankar.in@gmail.com>
2025-11-20 09:27:02 +0530

    commit

--- src/main/java/org/example/PetrolEngine.java
+++ src/main/java/org/example/PetrolEngine.java
@@ -1,8 +1,11 @@
 package org.example;
 
 public class PetrolEngine implements Engine{
-
+    public PetrolEngine(){
+        System.out.println("Petrol constructor");
+    }
     public int start(){
+
         System.out.println("Trying to start the petrol engine...");
         return 1;
     }
